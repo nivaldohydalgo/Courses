4ff62062a457880a69c9f6749761651743a84e53
nivaldohydalgo <nivaldohydalgo@gmail.com>
2024-01-11 16:51:14 -0300

    commit 5

--- C_language/C_course/Aula_006_constantes.c
+++ C_language/C_course/Aula_006_constantes.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PI 3.1415
+
 int main() {
     int numero1 = 10;
     const int numero2 = 20;
     
     numero1 = 30;
     // numero2 = 40;    // essa atribuição dá erro
+
+    float f = PI + 1;
+    printf("PI = %f \n", PI);
+    printf("f  = %f \n", f);
+    // PI = 14.4;     // isso dá erro 
 
 }
